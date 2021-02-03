7b250db2b30b0ecc71ec3c374e6060836fcbe6e7
Cat <CGraham@CatherinesMBP2.attlocal.net>
2021-02-03 10:03:27 -0600

    userin chema added

---
+++ server/schema/resolvers.js
@@ -0,0 +1,16 @@
+const { User, Thought } = require('../models');
+
+const resolvers = {
+  Query: {
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    }
+  }
+};
+  
+  module.exports = resolvers;
+
+  // thoughts: async () => {
+  //   return Thought.find().sort({ createdAt: -1 });
+  // }
